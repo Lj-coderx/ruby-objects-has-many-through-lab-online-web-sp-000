787840406b76762cb7603bd00584d91aa630acba
Lj <tnedutsl457892@gmail.com>
2021-07-01 05:28:48 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,28 +1,30 @@
 class Patient
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-    attr_accessor :name
-    @@all = []
-
-    def initialize(name)
-        @name = name
-        @@all << self
-    end
-
-    def self.all 
-        @@all
-    end
-
-    def new_appointment(doctor, date)
-        Appointment.new(date, self, doctor)
-    end
-
-    def appointments
-        Appointment.all.select{|appt| appt.patient == self}
-    end
-
-    def doctors
-        appointments.map {|appt| appt.doctor}
-    end
-
-
+  def self.all
+    @@all
+  end
+  
+  # Given a doctor and date, creates a new appointment belonging to that patient
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+  
+  # Returns all appointments associated with this Patient
+  def appointments
+    Appointment.all.select { |appointment| appointment.patient == self }
+  end
+  
+  # Has many doctors through appointments
+  def doctors
+    appointments.map {|appointment| appointment.doctor }
+  end
+  
 end
