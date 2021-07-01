7a6ec9dfbcc0a2c63369c992ba427fc540484d2a
Lj <tnedutsl457892@gmail.com>
2021-07-01 05:27:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,31 @@
 class Artist
+  attr_reader :name
+  
   @@all = []
-  attr_accessor :name
-
+  
   def initialize(name)
     @name = name
-    self.class.all << self
+    @@all << self
   end
-
+  
+  # Knows about all artist instances
   def self.all
     @@all
   end
-
-  def new_song(title, genre)
-    Song.new(title, self, genre)
-  end
-
+    
+  # Returns all songs associated with this Artist
   def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
+    Song.all.select {|song| song.artist == self}
   end
-
+  
+  # Given a name and genre, creates new song associated w/ that artist
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
+  # Has many genres, through songs
   def genres
-    songs.map do |song|
-      song.genre
-    end
+    songs.map { |song| song.genre }
   end
-
+  
 end
